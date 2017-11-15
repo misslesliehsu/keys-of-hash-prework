8b673f3e5d4de4d7d1cfe68276b45fd79cc2d412
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-15 22:54:51 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(arguments*)
+#return array of all keys with values that match what's given
+    array = []
+    arguments.each do |value|
+      self.each |k, v|
+        if value == v
+        array << k
+      end
   end
 end
